f0446e0166cc6313467deaec4d6693b561240515
yishuixm <1830802211@qq.com>
2016-09-08 15:01:56 +0800

    更新订单编辑生成器

--- src/StringTool.php
+++ src/StringTool.php
@@ -237,14 +237,15 @@ class StringTool {
         }
     }
 
+    /**
+     * 生成订单编号
+     * @return string
+     */
     static public function  orderNumber(){
         //生成24位唯一订单号码，格式：YYYY-MMDD-HHII-SS-NNNN,NNNN-CC，其中：
         //YYYY=年份，MM=月份，DD=日期，HH=24格式小时，II=分，SS=秒，NNNNNNNN=随机数，CC=检查码
-        
+
         //订购日期
-
-        $order_date = date('Y-m-d');
-
         //订单号码主体（YYYYMMDDHHIISSNNNNNNNN）
 
         $order_id_main = date('YmdHis') . rand(10000000, 99999999);
@@ -263,7 +264,7 @@ class StringTool {
 
         //唯一订单号码（YYYYMMDDHHIISSNNNNNNNNCC）
 
-        $order_id = $order_id_main . str_pad((100 - $order_id_sum % 100) % 100, 2, '0', STR_PAD_LEFT);
+        return $order_id_main . str_pad((100 - $order_id_sum % 100) % 100, 2, '0', STR_PAD_LEFT);
     }
 
 }
